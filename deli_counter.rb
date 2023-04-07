ea35a5a4b041d381e580411bef3539dc79a2115d
Luna Seifert <thepuddingdeity@gmail.com>
2023-04-07 15:04:48 -0500

    commit tree afe66f95ce06f7186fbc6923aa3afb09e7d4da68

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = []
+
+def line(deli_line)
+    if deli_line.length == 0
+        puts "The line is currently empty."
+    else
+        line_message = "The line is currently:"
+        deli_line.each.with_index(1) {|name, index| line_message << " #{index}. #{name}"}
+        puts line_message
+    end
+end
+
+def take_a_number deli_line, new_customer
+    deli_line << new_customer
+    puts "Welcome, #{new_customer}. You are number #{deli_line.length} in line."
+end
+
+def now_serving deli_line
+    if deli_line.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli_line.first}."
+        deli_line.shift
+    end
+end
